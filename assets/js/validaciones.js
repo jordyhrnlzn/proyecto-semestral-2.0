fbf11c33e02020d272a34565a5484c6ce56d8645
jordyhrnlzn <jo.lizana@duocuc.cl>
2022-05-03 22:44:12 -0400

    3 validaciones a la contraseña
    aaa

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -4,7 +4,7 @@ const inputs = document.querySelectorAll('#formulario input');
 const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
+	password: /^[a-zA-Z0-9]{4,12}$/, // 4 a 12 digitos.
 }
 
 const campos = {
